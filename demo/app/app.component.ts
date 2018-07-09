71c9917ea9348d8856816aa185d7198b9f1056f4
Alexandre Silva <alexandre.silva@kmart.com.au>
2018-07-09 17:34:15 +1000

    Implement payment notify functions

--- demo/app/app.component.ts
+++ demo/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, AfterViewChecked, OnInit, DoCheck, AfterViewInit } from "@angular/core";
-import { StripePaymentContext } from "nativescript-stripe-paymentcontext";
+import { Component } from "@angular/core";
+import { StripePaymentContext, STPEvents } from "nativescript-stripe-paymentcontext";
 import { StripeSettings } from "./stripe-settings";
 
 @Component({
@@ -17,6 +17,30 @@ export class AppComponent {
     constructor() {
         let settings: StripeSettings = require('./stripe-settings.json');
         this._stripe = new StripePaymentContext(settings.backendUrl, settings.publishableKey, settings.appleMerchantIdentifier);
+
+        // this._stripe.on(STPEvents.paymentContextDidChange, (event: any) => {
+        this._stripe.on("paymentContextDidChange", (event: any) => {
+            console.log(" >>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>> STPEvents.paymentContextDidChange");
+            // console.dir(event);
+        });
+
+        // this._stripe.on(STPEvents.paymentContextDidCreatePaymentResultCompletion, (event: any) => {
+        this._stripe.on("paymentContextDidCreatePaymentResultCompletion", (event: any) => {
+            console.log(" >>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>> STPEvents.paymentContextDidCreatePaymentResultCompletion");
+            console.dir(event);
+        });
+
+        // this._stripe.on(STPEvents.paymentContextDidFailToLoadWithError, (event: any) => {
+        this._stripe.on("paymentContextDidFailToLoadWithError", (event: any) => {
+            console.log(" >>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>> STPEvents.paymentContextDidFailToLoadWithError");
+            console.dir(event);
+        });
+
+        // this._stripe.on(STPEvents.paymentContextDidFinishWithError, (event: any) => {
+        this._stripe.on("paymentContextDidFinishWithError", (event: any) => {
+            console.log(" >>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>>>>> >>>>>>>>>>>>>>> STPEvents.paymentContextDidFinishWithError");
+            console.dir(event);
+        });
     }
 
     checkStripeHostViewController() {
@@ -34,6 +58,7 @@ export class AppComponent {
 
     request_payment() {
         this.checkStripeHostViewController();
+        this._stripe.paymentContext.paymentAmount = 500;
         this._stripe.paymentContext.requestPayment();
     }
 }
